0a3646d8f5bada395e6c2f7e1fabbb1047078379
PhuocSu <phuoclanh78@gmail.com>
2026-07-02 00:07:17 +0700

    feat(backend): update backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -27,6 +27,13 @@ app.use(cors({
     origin: process.env.CLIENT_URL
 }))
 
+// CLOUDINARY Configuration
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 //Swagger
 const swaggerDocument = JSON.parse(fs.readFileSync("./src/swagger.json", 'utf8'))
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument))
